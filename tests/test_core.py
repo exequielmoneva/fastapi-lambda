5bd7e7d246f9a6214eee13f21842219b7e660f52
exequielmoneva <exequielmoneva@gmail.com>
2022-11-03 23:57:00 -0300

    connection to DynamoDB

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,5 +7,5 @@ client = TestClient(app=app)
 
 def test_root():
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.status_code == 404
+    # assert response.json() == {"message": "Hello World"}
